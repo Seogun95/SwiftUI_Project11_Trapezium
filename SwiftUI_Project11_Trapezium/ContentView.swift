0f82dcf03b30c149d63c7aef1404688500b2989f
SeonJungKim <ksj083895@gmail.com>
2021-04-23 21:50:44 +0500

    1. 사다리꼴 만들기

--- SwiftUI_Project11_Trapezium/ContentView.swift
+++ SwiftUI_Project11_Trapezium/ContentView.swift
@@ -7,10 +7,40 @@
 
 import SwiftUI
 
+struct Trapezium: Shape {
+    
+    var offset: CGFloat = 100
+    
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        
+        path.move(to: .zero)
+        path.addLine(to: CGPoint(x: rect.maxX, y: 0))
+        path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
+        path.addLine(to: CGPoint(x: 0, y: offset))
+        path.closeSubpath()
+        
+        return path
+    }
+}
+
+// MARK : body
+
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+
+        VStack(spacing: 50) {
+            Trapezium(offset: 300)
+                .fill(Color.pink)
+                .frame(height: 400)
+                .edgesIgnoringSafeArea(.all)
+                
+            
+            Text("서근 개발블로그")
+                .font(.title)
+            
+                Spacer()
+        }
     }
 }
 
