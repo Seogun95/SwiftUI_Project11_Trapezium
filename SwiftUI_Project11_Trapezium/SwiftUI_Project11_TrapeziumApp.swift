a61ff348ad710327b8263cc132952fee2dde3740
SeonJungKim <ksj083895@gmail.com>
2021-04-23 21:16:22 +0500

    Initial Commit

---
+++ SwiftUI_Project11_Trapezium/SwiftUI_Project11_TrapeziumApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Project11_TrapeziumApp.swift
+//  SwiftUI_Project11_Trapezium
+//
+//  Created by 김선중 on 2021/04/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Project11_TrapeziumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
