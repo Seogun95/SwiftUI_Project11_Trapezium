4c08bd899e5f861da460250f44e002d21999e1e0
SeonJungKim <ksj083895@gmail.com>
2021-04-24 19:22:28 +0500

    완성

--- SwiftUI_Project11_Trapezium/ContentView.swift
+++ SwiftUI_Project11_Trapezium/ContentView.swift
@@ -9,15 +9,27 @@ import SwiftUI
 
 struct Trapezium: Shape {
     
-    var offset: CGFloat = 100
+    var offset: CGFloat = 0.8
+    var corner: UIRectCorner = .bottomLeft
+    
+    var animatableData: CGFloat {
+        get { return offset }
+        set { offset = newValue }
+    }
     
     func path(in rect: CGRect) -> Path {
         var path = Path()
         
-        path.move(to: .zero)
-        path.addLine(to: CGPoint(x: rect.maxX, y: 0))
-        path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
-        path.addLine(to: CGPoint(x: 0, y: offset))
+        // 시작점 - TopLeft
+        //Y좌표에서 cornerdl .topLeft라면 rect.maxy * offset
+        path.move(to: CGPoint(x: 0, y: corner == .topLeft ? rect.maxY * offset : 0 ))
+        // TopRight
+        path.addLine(to: CGPoint(x: rect.maxX, y: corner == .topRight ? rect.maxY * offset : 0))
+        // Bottom Right
+        path.addLine(to: CGPoint(x: rect.maxX, y: corner == .bottomRight ? rect.maxY * offset : rect.maxY))
+        
+        path.addLine(to: CGPoint(x: 0, y: corner == .bottomLeft ? rect.maxY * offset : rect.maxY))
+        
         path.closeSubpath()
         
         return path
@@ -27,19 +39,54 @@ struct Trapezium: Shape {
 // MARK : body
 
 struct ContentView: View {
+    
+    @State private var startChange = false
+    
     var body: some View {
-
+        
         VStack(spacing: 50) {
-            Trapezium(offset: 300)
-                .fill(Color.pink)
-                .frame(height: 400)
-                .edgesIgnoringSafeArea(.all)
+            
+            ZStack {
+                Trapezium(offset: 0.8, corner: .bottomRight)
+                    .fill(Color.yellow.opacity(0.5))
+                    .shadow(radius: 10)
+                    .frame(height: 400)
                 
+                Image("mountain")
+                    .resizable()
+                    .scaledToFill()
+                    .frame(height: 400)
+                    .clipShape(Trapezium(offset: startChange ? 0.5 : 1.1 ))
+                
+            }
+            .edgesIgnoringSafeArea(.top)
+            
             
             Text("서근 개발블로그")
-                .font(.title)
+                .font(.largeTitle)
+                .fontWeight(.bold)
             
-                Spacer()
+            
+            Spacer()
+            
+            
+            ZStack {
+                Trapezium(offset: 0.5, corner: .topLeft)
+                    .fill(Color.yellow.opacity(0.5))
+                    .shadow(radius: 10)
+                
+                Trapezium(offset: 0.5, corner: .topRight)
+                    .fill(Color.yellow.opacity(0.5))
+                    .shadow(radius: 10)
+            }
+            .edgesIgnoringSafeArea(.bottom)
+        }
+        //앱이 실행될때 구현
+        .onAppear {
+            //애니메이션 적용
+            withAnimation(Animation.linear(duration: 1)) {
+                self.startChange = true
+            }
         }
     }
 }
